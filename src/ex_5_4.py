f82c0e58c0f111aa17ddef0c4fc4238e671bfe97
vkudikal <164584629+vkudikal@users.noreply.github.com>
2024-05-03 14:07:05 -0500

    Update ex_5_4.py
    tried once again

--- src/ex_5_4.py
+++ src/ex_5_4.py
@@ -1,1 +1,24 @@
+"""ex_5_4.py"""
+import numpy as np
+from pathlib import Path
+
+try:
+    from src.util import get_repository_root
+except ImportError:
+    from util import get_repository_root
+
+# Use these predefined paths.  Note: automated tests expect these paths
+# Changing these paths will cause tests to fail.
+
+root_dir = get_repository_root()
+data_dir = root_dir / "data"
+output_dir = root_dir / "outputs"
+input_file = data_dir / "ex_5_4-data.csv"
+output_file = output_dir / "ex_5_4-processed.csv"
+
+data_in_file = np.loadtxt(input_file)
+
+data_in_file[data_in_file<0] = 0
+
+np.savetxt(output_file, data_in_file, fmt='%.2e')
 
